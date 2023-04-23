313befd449af447085607d124000978cb6bfe7ad
i-swinton <118025899+i-swinton@users.noreply.github.com>
2023-04-23 14:41:21 -0700

    Added board tile. Updated git ignores
    Updated the gitignore. Added a class for handling the board and boardtiles using graph for better coding experience

---
+++ Gamble2/Assets/Resources/Maps/BoardTile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MapSystem
+{
+
+    public class BoardTile : Node
+    {
+        public BoardTile(int id): base(id)
+        {
+
+        }
+    }
+}
