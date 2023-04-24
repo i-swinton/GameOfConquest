18e34b30bdea824a5dac3d710f11d76729bccda6
ZevTheBunny <47234179+ZevTheBunny@users.noreply.github.com>
2023-04-24 12:42:03 -0700

    Tiles Have Names

--- Gamble2/Assets/Zev's Chaos Corner/GenerateMap.cs
+++ Gamble2/Assets/Zev's Chaos Corner/GenerateMap.cs
@@ -23,9 +23,9 @@ public class GenerateMap : MonoBehaviour
             for (int j = 0; j < Contenents[i].Tiles.Length; j++)
             {
                 GameObject tile = Instantiate(TilePrefab, transform);
-                tile.GetComponent<SpriteRenderer>().sprite = Contenents[i].Tiles[j];
+                tile.GetComponent<SpriteRenderer>().sprite = Contenents[i].Tiles[j].Image;
                 tile.AddComponent<PolygonCollider2D>();
-                tile.GetComponent<MapTile>().NodeRef = (MapSystem.BoardTile)BoardManager.instance.GetBoard()[j];
+                tile.GetComponent<MapTile>().GenTile(BoardManager.instance.GetBoard()[j],Contenents[i].Tiles[j].Name);
                 tiles.Add(j);
             }
 
@@ -48,5 +48,13 @@ public class MapContinent
 {
     public string Name;
     public List<BonusBase> bonus;
-    public Sprite[] Tiles;
+    public TileData[] Tiles;
+}
+
+[System.Serializable]
+public struct TileData
+{
+    public string Name;
+    public Sprite Image;
+  //  Sprite this[int i = 0] { get { return Image; } }
 }
--- Gamble2/Assets/Zev's Chaos Corner/MapTile.cs
+++ Gamble2/Assets/Zev's Chaos Corner/MapTile.cs
@@ -1,24 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class MapTile : MonoBehaviour
 {
-    public string Player = "This is a temp variable";
+    public Player Player = null;
+    private string Name;
+
     public int Units = 1;
 
 
     public MapSystem.BoardTile NodeRef;
+    PolygonCollider2D Colid;
+    public Vector3 Center = new Vector3(1,1,1);
+
+    public Transform UIDisplays;
+    public TextMeshPro NameDisplay;
+    public TextMeshProUGUI UnitsDisplay;
+
 
     // Start is called before the first frame update
-    void Start()
+    public void GenTile(MapSystem.BoardTile nodeRef, string name)
     {
-        
+        NodeRef = nodeRef;
+        Name = name;
+
+        Colid = GetComponent<PolygonCollider2D>();
+
+        Center = Colid.bounds.center;
+
+        UIDisplays.position = Center + Vector3.back;
+        NameDisplay.text = Name;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(Center, 0.5f);
     }
 }
