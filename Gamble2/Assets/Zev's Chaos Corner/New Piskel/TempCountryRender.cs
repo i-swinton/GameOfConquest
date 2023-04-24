8f596831619d597f747c7504042f30d50d63134b
ZevTheBunny <47234179+ZevTheBunny@users.noreply.github.com>
2023-04-24 16:19:54 -0700

    Added Tile shadder

--- Gamble2/Assets/Zev's Chaos Corner/New Piskel/TempCountryRender.cs
+++ Gamble2/Assets/Zev's Chaos Corner/New Piskel/TempCountryRender.cs
@@ -5,33 +5,73 @@ using UnityEngine;
 public class TempCountryRender : MonoBehaviour
 {
     SpriteRenderer Renderer;
+    public Material Mat;
+    public MaterialPropertyBlock Block;
+    public bool Heilight;
+    public enum SelectState
+    {
+        CannotSelect,
+        CanSelect,
+        Selected,
+        OtherSelected
+    }
+    public SelectState _State;
+
+    public SelectState State { get{
+            return _State;
+        }
+        set
+        {
+            _State = value;
+            Mat.SetFloat("_State", (float)State);
+            Debug.Log("State Change", this);
+
+        }
+    }
+    /*
+     * Cannot Select
+     * Can Select
+     * Selected
+     * OtherSelected
+     */
 
     public Color Hilighted = Color.gray;
     public Color Default = Color.white;
     // Start is called before the first frame update
     void Awake()
     {
+        Mat = new Material(Mat);
+        Block = new MaterialPropertyBlock();
         Renderer = GetComponent<SpriteRenderer>();
+        Renderer.material = Mat;
+
+        Mat.SetColor("_PlayerColor", Color.green);
+      //  Renderer.SetPropertyBlock(Block);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SetHeilight(bool value)
     {
         if (value)
         {
-            Renderer.color = Hilighted;
-        }
+            State = SelectState.CanSelect;}
         else
-            Renderer.color = Default;
+        {
+            State = SelectState.CannotSelect;
+        }
+        //Renderer.color = Default;
     }
     public void DebugLog()
     {
+        State = SelectState.Selected;
         MapTile mt = GetComponent<MapTile>();
+
 
         for(int i=0; i < mt.NodeRef.Parent.ContinentCount; ++i)
         {
