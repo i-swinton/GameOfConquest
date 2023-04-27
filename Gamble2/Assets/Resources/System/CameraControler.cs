e3daedbf28522652650e6bcc1fa31596e4e8fe91
ZevTheBunny <47234179+ZevTheBunny@users.noreply.github.com>
2023-04-27 16:51:20 -0700

    Improved Camera Pan/Zoom

--- Gamble2/Assets/Resources/System/CameraControler.cs
+++ Gamble2/Assets/Resources/System/CameraControler.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class CameraControler : MonoBehaviour
 {
-    [Header("Scroll Settings")]
+
+    [Header("Zoom Settings")]
     public float MaxHeight;
     public float MinHeight;
     [Tooltip("Speed camera zooms. Negative inverts")]
     public float ZoomSpeed;
-    
+
+    [SerializeField] Camera TileUICam;
+    [SerializeField] float MaskDistanceThreshold;
+    [SerializeField] LayerMask CloseMask;
+    [SerializeField] LayerMask DistanceMask;
+
+
     [Header("Pan Settings")]
     [Tooltip("Speed the camera can pan. Negative inverts")]
     public float PanSpeed;
 
     Vector3 PreMosPos;
-
 
     // Update is called once per frame
     void Update()
@@ -52,6 +58,13 @@ public class CameraControler : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, MaxHeight);
         else if (transform.position.z < MinHeight)
             transform.position = new Vector3(transform.position.x, transform.position.y, MinHeight);
+
+
+        //Turns on and off tile names based off distance.
+        if (transform.position.z >= MaskDistanceThreshold)
+            TileUICam.cullingMask = CloseMask;
+        else
+            TileUICam.cullingMask = DistanceMask;
 
     }
 }
