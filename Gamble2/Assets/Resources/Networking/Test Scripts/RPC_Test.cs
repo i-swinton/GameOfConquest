40ab6f662e7f276f212b7e77840b35393d07a766
i-swinton <118025899+i-swinton@users.noreply.github.com>
2023-04-30 20:20:04 -0700

    Update RPC_Test.cs

--- Gamble2/Assets/Resources/Networking/Test Scripts/RPC_Test.cs
+++ Gamble2/Assets/Resources/Networking/Test Scripts/RPC_Test.cs
@@ -17,18 +17,19 @@ public class RPC_Test : NetworkBehaviour
     void TestClientRpc(int value, ulong sourceNetworkObjectId)
     {
         DebugNetworklLog.Log($"Client Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
-        NetworkConsole.instance.SetText($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
+        //NetworkConsole.instance.SetText($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
         if (IsOwner) //Only send an RPC to the server on the client that owns the NetworkObject that owns this NetworkBehaviour instance
         {
             TestServerRpc(value + 1, sourceNetworkObjectId);
         }
     }
 
+    
     [ServerRpc]
     void TestServerRpc(int value, ulong sourceNetworkObjectId)
     {
         DebugNetworklLog.Log($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
-        NetworkConsole.instance.SetText($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
+        //NetworkConsole.instance.SetText($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
         TestClientRpc(value, sourceNetworkObjectId);
     }
 }
